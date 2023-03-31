cc26e7f36c889b8ba13bf739be0f9d3149f94990
Pedro Correia <pedropmpc@gmail.com>
2023-03-31 11:34:47 +0100

    aa

--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -2,7 +2,7 @@ const commerceAPI = require('../../api/commerceAPI');
 
 exports.validateLogin = () => {
     return async (req, res, next) => {
-        // Getting customer info - LOGDETAILS
+        // Getting customer data
         const Customer = await commerceAPI.getCustomerLogin(req.body.email, req.body.password);
         var findFlag = false;
         if (Customer == 'notFound') {
@@ -11,15 +11,17 @@ exports.validateLogin = () => {
             req.session.regenerate(function (err) {
                 if (err) next(err);
 
-                // store user information in session, typically a user id
+                // store user information in session
                 req.session.customer = Customer;
                 req.session.isLogged = true;
+                //Flag user sucessfully logged in
                 findFlag = true;
 
                 // save the session before redirection to ensure page
                 // load does not happen before session is saved
             });
         }
+        //Saving new session
         req.session.save(function (err) {
             if (err) return next(err);
             res.json({ customer: Customer, findFlag: findFlag });
