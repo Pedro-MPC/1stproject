87b0fb86a0516cf7f50fe58d16fd9875d9341387
Pedro Correia <pedropmpc@gmail.com>
2023-04-04 12:48:43 +0100

    a

--- src/models/productcopy/product.js
+++ src/models/productcopy/product.js
@@ -1,10 +1,14 @@
+const productPDP = require('./decorators/productPDP');
+
 function Product(id, name, price, category) {
     this.id = id;
     this.name = name;
     this.price = price;
     this.category = category;
 }
-const newProduct = new Product(1, 'name', 'price', 'category');
+const baseProduct = new Product(1, 'nameA', 'priceA', 'categoryA');
+const decoratedProduct = new productPDP(baseProduct, 'descriptionA', 'imageA');
 
-console.log(newProduct);
-module.exports = newProduct;
+console.log(decoratedProduct);
+
+module.exports = Product;
