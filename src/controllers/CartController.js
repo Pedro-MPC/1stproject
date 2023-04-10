8c895ec4d992a3f93e9460df373d008dd8c24285
Pedro Correia <pedropmpc@gmail.com>
2023-04-10 17:42:07 +0100

    a

--- src/controllers/CartController.js
+++ src/controllers/CartController.js
@@ -7,13 +7,9 @@ exports.addToCart = () => {
         const PID = req.body.productId;
         const PRODUCT = await productModel.Product('tile', PID);
 
-        console.log(PRODUCT);
-
         if (!req.session.cart) {
             req.session.cart = [];
-            console.log('cart created');
         }
-        console.log(req.session.cart.length);
 
         if (req.session.cart.length > 0) {
             var flag = false;
@@ -26,15 +22,12 @@ exports.addToCart = () => {
                     }
                 });
             } catch (err) {
-                console.log('break');
                 if (err !== 'break') throw err;
             }
             if (!flag) {
-                console.log('FIrst entry of product');
                 req.session.cart.push({ PRODUCT: PRODUCT, quantity: 1 });
             }
         } else if (req.session.cart.length == 0) {
-            console.log('FIrst entry of product');
             req.session.cart.push({ PRODUCT: PRODUCT, quantity: 1 });
         }
 
@@ -64,7 +57,6 @@ exports.updateCartHTML = () => {
                 Total += cart.PRODUCT.price * cart.quantity;
             });
             req.session.cart.total = Total;
-            console.log(req.session);
             res.render('partials/cart-items', {
                 CartItem: CartItem,
                 Total: Total,
--- src/routes/Account.js
+++ src/routes/Account.js
@@ -5,7 +5,7 @@ const accController = require('../controllers/AccountController');
 /** Account Routes */
 
 /** Running on every request */
-router.get('*', accController.checkSession(), accController.globalAccountVariables());
+router.get('*', accController.checkSession());
 router.post('/validateLogin', accController.validateLogin());
 router.post('/registercustomer', accController.checkEmailExist(), accController.registerCustomer());
 router.post('/logout', accController.logout());
