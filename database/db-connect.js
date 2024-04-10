1a43eecae3a710bfa83cd962a88d3d7946b6b6bd
Pedro Correia <pedropmpc@gmail.com>
2024-04-11 00:47:19 +0100

    Update db-connect.js

--- database/db-connect.js
+++ database/db-connect.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
     host: 'srv507226',
-    user: 'root@localhost',
-    password: 'pedro',
+    user: 'root',
+    password: 'NewPassword123!', // Adjust this password to meet policy requirements
     database: 'nodeProd'
 });
 
