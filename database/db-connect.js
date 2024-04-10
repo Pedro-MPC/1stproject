b17fab5431b4a1892ea1d644fe61a769554fdf9e
Pedro Correia <pedropmpc@gmail.com>
2024-04-11 00:41:28 +0100

    Update db-connect.js

--- database/db-connect.js
+++ database/db-connect.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: '91.108.102.28',
     user: 'root',
     password: 'pedro',
     database: 'nodeProd'
