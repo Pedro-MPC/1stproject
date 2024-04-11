57aa84a3030fb2521105a4564b7eb3547122b936
Pedro Correia <pedropmpc@gmail.com>
2024-04-11 03:06:05 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -71,7 +71,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(app);
+  res.end('teste');
 });
 
 server.listen(port, hostname, () => {
