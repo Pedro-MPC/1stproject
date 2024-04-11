830824106e24df2126f4b40a8d29566bc88432d2
Pedro Correia <pedropmpc@gmail.com>
2024-04-11 13:12:01 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,42 +9,42 @@ const app = express();
 app.use(express.static('src/public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
-// app.use(bodyParser.json());
-// app.use(cookieParser('fsgdesgsdYYFCCXXX'));
-// app.use(
-//     session({
-//         secret: ['veryimportantsecret', 'notsoimportantsecret', 'highlyprobablysecret'],
-//         name: 'secretname',
-//         cookie: {
-//             httpOnly: true,
-//             sameSite: true,
-//             maxAge: 86400000 // Time is in milliseconds
-//         }
-//     })
-// );
-// app.use(csrf({ cookie: true }));
+app.use(bodyParser.json());
+app.use(cookieParser('fsgdesgsdYYFCCXXX'));
+app.use(
+    session({
+        secret: ['veryimportantsecret', 'notsoimportantsecret', 'highlyprobablysecret'],
+        name: 'secretname',
+        cookie: {
+            httpOnly: true,
+            sameSite: true,
+            maxAge: 86400000 // Time is in milliseconds
+        }
+    })
+);
+app.use(csrf({ cookie: true }));
 
-// // Locals Middleware
-// app.use(function (req, res, next) {
-//     res.locals.csrfToken = req.csrfToken();
-//     next();
-// });
+// Locals Middleware
+app.use(function (req, res, next) {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
 
-// // Routes
-// const homeRoute = require('./routes/homepage');
-// const productRoute = require('./routes/Product');
+// Routes
+const homeRoute = require('./routes/homepage');
+const productRoute = require('./routes/Product');
 const errorRoute = require('./routes/404');
-// const accountRoute = require('./routes/Account');
-// const cartRoute = require('./routes/Cart');
-// const categoryRoute = require('./routes/Category');
-// const CheckoutRoute = require('./routes/Checkout');
+const accountRoute = require('./routes/Account');
+const cartRoute = require('./routes/Cart');
+const categoryRoute = require('./routes/Category');
+const CheckoutRoute = require('./routes/Checkout');
 
-// app.use(cartRoute);
-// app.use(productRoute);
-// app.use(homeRoute);
-// app.use(accountRoute);
-// app.use(categoryRoute);
-// app.use(CheckoutRoute);
+app.use(cartRoute);
+app.use(productRoute);
+app.use(homeRoute);
+app.use(accountRoute);
+app.use(categoryRoute);
+app.use(CheckoutRoute);
 app.use('*', errorRoute);
 
 
