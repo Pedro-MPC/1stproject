8d962cf94c04d4d99dd0a0e2f9d08b2e2802841f
Pedro Correia <pedropmpc@gmail.com>
2024-04-11 13:15:44 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.use(function (req, res, next) {
 // app.use(accountRoute);
 // app.use(categoryRoute);
 // app.use(CheckoutRoute);
-// app.use('*', errorRoute);
+app.use('*', errorRoute);
 
 
 // Error Handling Middleware
